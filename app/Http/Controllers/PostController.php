931b5dc42f244c7a684e5c0f2b8d5e8b2797f1e0
mittelmabigkeit <comp422012016@mail.ru>
2016-11-25 17:54:03 +0300

    boom

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+        dd($posts);
+        return view('index');
+    }
+
+    public function create()
+    {
+      //
+    }
+
+    public function store()
+    {
+
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,12 +1,12 @@
 <?php
+//use App\Http\Controllers;
 
-Route::get('/', function () {
-    return View::make('home.index');
-});
+//Route::get('/', 'PostController@index');
 
+Route::get('/', ['as' => 'test', 'uses' => 'PostController@index']);
 
-Route::get('/tasks', function () {
-    return json_encode(app\Models\Mission::all() );
-});
+//Route::get('/tasks', function () {
+  //  return json_encode(app\Models\Mission::all() );
+//});
 
 ?>
